e7a6110585765343ddf5202f9a3ef8f3486dc9b4
Aqqil Azman <aqqil.azman99@gmail.com>
2023-01-09 09:55:07 +0800

    detect current coordinate

--- main.js
+++ main.js
@@ -1,22 +1,28 @@
-var url = "https://en.wikipedia.org/w/api.php"; 
-
-var params = {
-    action: "query",
-    prop: "coordinates",
-    titles: "Wikimedia Foundation",
-    format: "json"
-};
-
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-
-fetch(url)
-    .then(function(response){return response.json();})
-    .then(function(response) {
-        var pages = response.query.pages;
-        for (var page in pages) {
-            console.log("Latitute: " + pages[page].coordinates[0].lat);
-            console.log("Longitude: " + pages[page].coordinates[0].lon);
-        }
-    })
-    .catch(function(error){console.log(error);});
+navigator.geolocation.getCurrentPosition(function(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+  
+    var url = "https://en.wikipedia.org/w/api.php"; 
+  
+    var params = {
+        action: "query",
+        prop: "coordinates",
+        titles: "Wikimedia Foundation",
+        format: "json"
+    };
+  
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+  
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            var pages = response.query.pages;
+            for (var page in pages) {
+                console.log("Latitute: " + lat);
+                console.log("Longitude: " + lon);
+            }
+        })
+        .catch(function(error){console.log(error);});
+  });
+  
