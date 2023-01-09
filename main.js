4b5e6b1a669a8793d7a7f8537be7580ce4a09d70
Aqqil Azman <aqqil.azman99@gmail.com>
2023-01-09 10:28:43 +0800

    added Get Location Button

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-navigator.geolocation.getCurrentPosition(function(position) {
+var url = "https://en.wikipedia.org/w/api.php"; 
+
+var params = {
+    action: "query",
+    prop: "coordinates",
+    titles: "Wikimedia Foundation",
+    format: "json"
+};
+
+url = url + "?origin=*";
+Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+var button = document.getElementById("get-location-button");
+button.addEventListener("click", function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
 
-    var coordinatesDiv = document.getElementById("coordinates");
-    coordinatesDiv.innerHTML = "Latitude: " + lat + ", Longitude: " + lon;
-  
-    var url = "https://en.wikipedia.org/w/api.php"; 
-  
-    var params = {
-        action: "query",
-        prop: "coordinates",
-        titles: "Wikimedia Foundation",
-        format: "json"
-    };
-  
-    url = url + "?origin=*";
-    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-  
     fetch(url)
         .then(function(response){return response.json();})
         .then(function(response) {
@@ -24,8 +23,10 @@ navigator.geolocation.getCurrentPosition(function(position) {
             for (var page in pages) {
                 console.log("Latitute: " + lat);
                 console.log("Longitude: " + lon);
+                var coordinatesDiv = document.getElementById("coordinates");
+                coordinatesDiv.innerHTML = "Latitude: " + lat + ", Longitude: " + lon;
             }
         })
         .catch(function(error){console.log(error);});
   });
-  
+});
