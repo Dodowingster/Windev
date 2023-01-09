ca04b5f327f5673200e1f2cb89fd1ab8433aad68
Aqqil Azman <aqqil.azman99@gmail.com>
2023-01-09 10:34:39 +0800

    display list of locations nearby

--- main.js
+++ main.js
@@ -1,31 +1,33 @@
-var url = "https://en.wikipedia.org/w/api.php"; 
-
-var params = {
-    action: "query",
-    prop: "coordinates",
-    titles: "Wikimedia Foundation",
-    format: "json"
-};
-
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-
 var button = document.getElementById("get-location-button");
 button.addEventListener("click", function() {
   navigator.geolocation.getCurrentPosition(function(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
 
+    var url = "https://en.wikipedia.org/w/api.php"; 
+
+    var params = {
+        action: "query",
+        list: "geosearch",
+        gscoord: lat + "|" + lon,
+        gsradius: "10000",
+        gslimit: "10",
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
     fetch(url)
         .then(function(response){return response.json();})
         .then(function(response) {
-            var pages = response.query.pages;
-            for (var page in pages) {
-                console.log("Latitute: " + lat);
-                console.log("Longitude: " + lon);
-                var coordinatesDiv = document.getElementById("coordinates");
-                coordinatesDiv.innerHTML = "Latitude: " + lat + ", Longitude: " + lon;
+            var locations = response.query.geosearch;
+            var locationsList = "";
+            for (var place in locations) {
+                locationsList += "<li>" + locations[place].title + "</li>";
             }
+            var locationsDiv = document.getElementById("locations");
+            locationsDiv.innerHTML = "<ul>" + locationsList + "</ul>";
         })
         .catch(function(error){console.log(error);});
   });
