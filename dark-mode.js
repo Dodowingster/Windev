0c67808fbfdaea811e7f86b012f0e587356f3946
Aqqil Azman <aqqil.azman99@gmail.com>
2023-05-31 22:09:12 +0800

    cleaning css styles

--- dark-mode.js
+++
@@ -1,6 +0,0 @@
-const darkModeButton = document.getElementById("dark-mode-button");
-const body = document.body;
-
-darkModeButton.addEventListener("click", function() {
-  body.classList.toggle("dark-mode");
-});
